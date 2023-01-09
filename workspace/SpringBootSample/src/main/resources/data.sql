dfd63c8a4ad86b2e8e9b37049c459e3e25327472
yamamotodaiki <yamamotodaiki@localhost>
2023-01-09 10:31:30 +0900

    dbの初期データ、スキーマを作成クエリ追加

--- workspace/SpringBootSample/src/main/resources/data.sql
+++ workspace/SpringBootSample/src/main/resources/data.sql
@@ -1,2 +1,17 @@
 INSERT INTO employee (id, name, age)
-VALUES('1', 'TOM', 30);
+VALUES('1', 'TOM', 30) ON DUPLICATE KEY UPDATE id=id;
+
+/* ユーザーマスター */
+INSERT INTO m_user (
+user_id
+,password
+,user_name
+,birthday
+,age
+,gender
+,department_id
+,role
+) VALUES
+('system@co.jp','password','システム管理者','2000-01-01',21,1,1,'ROLE_ADMIN')
+,('user@co.jp','password', 'ユーザー1','2000-01-01',21,2,2,'ROLE_GENERAL')
+ON DUPLICATE KEY UPDATE user_id=user_id;
