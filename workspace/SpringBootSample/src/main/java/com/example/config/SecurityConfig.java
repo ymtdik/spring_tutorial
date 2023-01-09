fa8c8a8626e86171f5f79a9bbecd3659f21990bc
yamamotodaiki <yamamotodaiki@localhost>
2023-01-09 11:36:51 +0900

    インメモリ認証

--- workspace/SpringBootSample/src/main/java/com/example/config/SecurityConfig.java
+++ workspace/SpringBootSample/src/main/java/com/example/config/SecurityConfig.java
@@ -5,11 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+	
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
@@ -30,5 +40,13 @@ public class SecurityConfig {
 				.anyRequest().authenticated());
 		http.csrf().disable();
 		return http.build();
+	}
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		PasswordEncoder encoder = passwordEncoder();
+        UserDetails user = User.withUsername("user").password(encoder.encode("user")).roles("GENERAL").build();
+        UserDetails admin = User.withUsername("admin").password(encoder.encode("admin")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
 	}
 }
